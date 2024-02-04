383bee78331ac77d8444f62ad8474ed979cb6ba2
Pedro Boni Gasparotto <pedrobg2707@gmail.com>
2024-02-04 20:51:11 -0300

    trabalhando nos html

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from contact import views
 
-app_name = 'contact'
+app_name = 'contact' # isso é o primeiro parametro do link id da tabela em index.html >>contact<<:contact
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact' ),
     path('', views.index, name='index'),
 ]
+
+# name='contact' é o segundo parametro do link id da tabela em index.html contact:>>contact<<
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,7 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
+from contact.models import Contact
 
 # Create your views here.
 def index(request):
+
+    contacts = Contact.objects.filter(show=True).order_by('-id')[10:20]
+
+    context = {
+        'contacts' : contacts,
+    }
+
     return render(
-        request,'contact/index.html'
+        request,'contact/index.html', context
+    )
+
+def contact(request, contact_id):
+
+    single_contact = Contact.objects.filter(pk=contact_id, show=True).first() # não estou utilizando get() pois ele retornaria um erro caso eu busque, na url, um valor que não exista ou um valor duplicado.
+
+    if single_contact is None:
+        raise Http404()
+    
+    # O trecho acima tb funcionaria assim:
+    # single_contact = get_object_or_404(
+    # Contact.objects.filter(pk=contact_id))
+    
+    # ou assim:
+    # single_contact = get_object_or_404(
+    # Contact.objects, pk=contact_id)
+
+    # ou assim:
+    # single_contact = get_object_or_404(
+    # Contact, pk=contact_id)
+
+    context = {
+        'contact' : single_contact,
+    }
+
+    return render(
+        request,'contact/contact.html', context
     )
