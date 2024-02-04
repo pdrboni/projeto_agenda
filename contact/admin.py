8af1f02e96bff7cb184ececfec101539a465b976
Pedro Boni Gasparotto <pedrobg2707@gmail.com>
2024-02-04 12:52:58 -0300

    criando owner no model contact

--- contact/admin.py
+++ contact/admin.py
@@ -13,4 +13,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_max_show_all = 200
     #list_editable = 'first_name', 'last_name', 
-    list_display_links = 'first_name', 'id', 
+    list_display_links = 'first_name', 'id',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
