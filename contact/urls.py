e25ae401c3bbfa038adb47c9e47c12d8340818e1
Pedro Boni Gasparotto <pedrobg2707@gmail.com>
2024-02-05 01:33:34 -0300

    crud+paginacao+busca

--- contact/urls.py
+++ contact/urls.py
@@ -4,8 +4,12 @@ from contact import views
 app_name = 'contact' # isso é o primeiro parametro do link id da tabela em index.html >>contact<<:contact
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact' ),
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+    #contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact' ),
+    path('contact/create/', views.create, name='create' ),
 ]
-
 # name='contact' é o segundo parametro do link id da tabela em index.html contact:>>contact<<
+
+    
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 from contact.models import Contact
 
 # Create your views here.
 def index(request):
 
-    contacts = Contact.objects.filter(show=True).order_by('-id')[10:20]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
 
     context = {
-        'contacts' : contacts,
+        'page_obj' : page_obj,
+        'site_title': 'Contatos - '
     }
 
     return render(
@@ -35,10 +43,36 @@ def contact(request, contact_id):
     # single_contact = get_object_or_404(
     # Contact, pk=contact_id)
 
+    contact_name = f'{single_contact.first_name} - '
+
     context = {
         'contact' : single_contact,
+        'site_title': contact_name,
     }
 
     return render(
         request,'contact/contact.html', context
+    )
+
+def search(request):
+
+    search_value = request.GET.get('q', '').strip() # esse GET vai ser um dicionario, sendo a chave o name do seu form (input em _header.html) e o valor sendo o valor que você procurou em search. o '.get' vai pegar o valor da cahve 'q' do seu dicionario e se não houver valor, ele retorna ''.
+    
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects.filter(show=True).filter(Q(first_name__icontains = search_value) | Q(last_name__icontains = search_value)| Q(phone__icontains = search_value)| Q(email__icontains = search_value)).order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj' : page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value
+    }
+
+    return render(
+        request,'contact/index.html', context
     )
